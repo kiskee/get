7d69ebd072bd213e34c5cb2ff54545ccf3ced484
danielmed <daniel.medina@talent.com>
2023-07-06 12:47:43 -0500

    adding get route

---
+++ app/api/users/route.js
@@ -0,0 +1,17 @@
+import connectMongoDB from "@/libs/mongodb"
+import User from "@/models/user"
+import { NextResponse} from "next/server"
+
+
+export async function POST(req){
+    const{username, password, email} = await req.json()
+    await connectMongoDB()
+    await User.create({username, password, email})
+    return NextResponse.json({message: "todo r"},{status: 200})
+}
+
+export async function GET() {
+    await connectMongoDB();
+    const users = await User.find();
+    return NextResponse.json({ users });
+  }
